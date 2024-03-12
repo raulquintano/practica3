f05a9591a808b0943a24ccb88790ceae3d68285a
raulquintano <raul.quintanovazquez@usp.ceu.es>
2024-03-12 08:55:38 +0100

    comentarios

--- src/aplicacion/Main.java
+++ src/aplicacion/Main.java
@@ -1,11 +1,14 @@
 package aplicacion;
 
+// Importamos la clase Operaciones del paquete mates
 import mates.Operaciones;
 
+// Clase Main
 public class Main {
 
+    // Metodo main
     public static void main(String[] args) {
-
+        // Imprimimos el resultado de la aproximacion al numero pi
         System.out.println("La aproximacion al numero pi es: " + Operaciones.aproximacionPi(Integer.parseInt("100000000")));
     }
 }
--- src/mates/Operaciones.java
+++ src/mates/Operaciones.java
@@ -1,13 +1,21 @@
 package mates;
+// Importamos la clase DoubleStream del paquete java.util.stream
 import java.util.stream.DoubleStream;
 
+// Clase Operaciones
 public class Operaciones {
 
+    // Metodo para calcular la aproximacion al numero pi
     public static double aproximacionPi(long pasos) {
+        // Calculamos la cantidad de puntos que caen dentro del circulo
         double puntos = DoubleStream.generate(() -> Math.random() * 2 - 1)
+        // Limitamos la cantidad de puntos a la cantidad de pasos
         .limit(pasos)
+        // Filtramos los puntos que caen dentro del circulo
         .filter(x -> Math.pow(x, 2) + Math.pow(Math.random() * 2 - 1, 2) <= 1)
+        // Contamos la cantidad de puntos que quedaron
         .count();
+        // Retornamos la aproximacion al numero pi
         return 4.0 * puntos / pasos;
     }
 }
