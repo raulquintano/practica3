19208d920e89dce82e9f023273bebd34a3db413b
raulquintano <raul.quintanovazquez@usp.ceu.es>
2024-03-15 13:45:31 +0100

    cambios finales

--- src/mates/Operaciones.java
+++ src/mates/Operaciones.java
@@ -1,19 +1,22 @@
 package mates;
-// Importamos la clase DoubleStream del paquete java.util.stream
-import java.util.stream.DoubleStream;
+
+// Importamos la clase Random del paquete java.util
+import java.util.Random;
+import java.util.stream.IntStream;
 
 // Clase Operaciones
 public class Operaciones {
 
+    // Creamos un objeto Random
+    static Random random = new Random();
+
     // Metodo para calcular la aproximacion al numero pi
     public static double aproximacionPi(long pasos) {
-        // Calculamos la cantidad de puntos que caen dentro del circulo
-        double puntos = DoubleStream.generate(() -> Math.random() * 2 - 1)
-        // Limitamos la cantidad de puntos a la cantidad de pasos
-        .limit(pasos)
+        // Calculamos los puntos que caen dentro del circulo
+        long puntos = IntStream.rangeClosed(0, (int) pasos)
         // Filtramos los puntos que caen dentro del circulo
-        .filter(x -> Math.pow(x, 2) + Math.pow(Math.random() * 2 - 1, 2) <= 1)
-        // Contamos la cantidad de puntos que quedaron
+        .filter(i -> Math.pow(random.nextDouble(), 2) + Math.pow(random.nextDouble(), 2) <= 1)
+        // Contamos los puntos que caen dentro del circulo
         .count();
         // Retornamos la aproximacion al numero pi
         return 4.0 * puntos / pasos;
